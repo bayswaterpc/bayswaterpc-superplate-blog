092a94217b3fabbfb91d42bef263cc329cd93baa
Jacob <bayswaterpc@gmail.com>
2021-09-12 20:00:59 -0400

    Upgrade next-bundle-analyzer and try to get bundle working

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-const withPlugins = require("next-compose-plugins");
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = withPlugins([], {});
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  // your other plugins here
+])
